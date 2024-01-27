a55374f71f173f4131ac85c120aae5c9c89487a5
motojima_yu <motojima_yu@example.com>
2024-01-28 01:49:57 +0900

    Adding .gitignore

--- src/config/Config.js
+++
@@ -1,9 +0,0 @@
-// Config is now Dummy.
-
-export const END_POINT = "https://gitlab.com/api/v4/projects/"
-export const ACCESS_TOKEN = "";
-export const PROJECT_ID = 00000000;
-export const BRANCH_LIST_URL = "https://gitlab.com/api/v4/projects/00000000/repository/branches";
-export const MEMBER_ALL_LIST_URL = "https://gitlab.com/api/v4/projects/00000000/members/all"
-export const MERGE_REQUEST_LIST_URL = "https://gitlab.com/api/v4/projects/00000000/merge_requests";
-export const MERGE_REQUEST_DISCUSSIONS_PATH = "/discussions"
